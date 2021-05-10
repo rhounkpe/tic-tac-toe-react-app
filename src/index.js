5be6983da97bc8d7fad637924af7fb26c0774e36
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-10 20:21:03 +0200

    Turn Square class into a function component

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ class Board extends React.Component {
     handleClick(i) {
         const squares = this.state.squares.slice();
         squares[i] = 'X';
-        this.setState({squares: squares});
+        this.setState({ squares: squares });
     }
 
     renderSquare(i) {
-        return (<Square 
+        return (<Square
             value={this.state.squares[i]}
             onClick={() => this.handleClick(i)}
         />);
@@ -26,41 +26,36 @@ class Board extends React.Component {
 
     render() {
         const status = 'Next player: X';
-    
+
         return (
-          <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-              {this.renderSquare(0)}
-              {this.renderSquare(1)}
-              {this.renderSquare(2)}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-              {this.renderSquare(3)}
-              {this.renderSquare(4)}
-              {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-              {this.renderSquare(6)}
-              {this.renderSquare(7)}
-              {this.renderSquare(8)}
-            </div>
-          </div>
         );
     }
 }
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick({value: 'X'}) }
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square"onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Game extends React.Component {
